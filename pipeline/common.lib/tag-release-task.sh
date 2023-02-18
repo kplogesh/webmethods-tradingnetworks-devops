70c9fbfb9fd93a308e55f6b222ba1bab0893f3df
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-18 13:27:46 +0530

    updates

--- pipeline/common.lib/tag-release-task.sh
+++ pipeline/common.lib/tag-release-task.sh
@@ -5,10 +5,10 @@ VERSION=$1
 git tag
 
 # Create the tag
-git tag -a ${VERSION} -m "Tag for release ${VERSION}"
+git tag -f -a ${VERSION} -m "Tag for release ${VERSION}"
 
 # Display the specific tag
 git show ${VERSION}
 
 # Push to the origin
-git push origin ${VERSION}
+git push -f origin ${VERSION}
