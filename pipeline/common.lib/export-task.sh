20e9280098d1c26cda5da7306f7eb37d69b7a0a9
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-18 12:51:34 +0530

    new updates

--- pipeline/common.lib/export-task.sh
+++ pipeline/common.lib/export-task.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 kubectl config set-context --current --namespace=$3
-
 cd applications/tradingnetworks/manifests/jobs
 # Modify the k8s job name with release iteration and apply the k8s job specifications 
 VERSION=$1
 sed -i "s/<TAG>/${VERSION}/g" tn-assetexport-job.yaml
 sed -i "s/<TAG>/${VERSION}/g" tn-importexportscript-cm.yaml
-
+ 
 # Apply the kubernetes specifications to create the required assets
 kubectl apply -f ../../env-manifests/$2/tn-appprop-cm.yaml -f ../../env-manifests/$2/webmethods-licenses.yaml -f ../tn-utilfiles-cm.yaml -f tn-importexportscript-cm.yaml -f tn-assetexport-job.yaml
 
