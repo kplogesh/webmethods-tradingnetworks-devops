101f114c53e7d3d248645a9455e4c9956aed655a
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-18 08:14:09 +0530

    updates

--- pipeline/common.lib/export-task.sh
+++ pipeline/common.lib/export-task.sh
@@ -8,8 +8,8 @@ MINOR=`sed -n 's/^MINOR=\(.*\)/\1/p' < ../../version.txt`
 PATCH=`sed -n 's/^PATCH=\(.*\)/\1/p' < ../../version.txt`
 VERSION=v${MAJOR}.${MINOR}.${PATCH}
 
-sed -i "s/<TAG>/${VERSION}/" tn-assetexport-job.yaml
-sed -i "s/<TAG>/${VERSION}/" tn-importexportscript-cm.yaml
+sed -i "s/<TAG>/${VERSION}/g" tn-assetexport-job.yaml
+sed -i "s/<TAG>/${VERSION}/g" tn-importexportscript-cm.yaml
 
 # Apply the kubernetes specifications to create the required assets
 kubectl apply -f ../../env-manifests/$2/tn-appprop-cm.yaml -f ../../env-manifests/$2/webmethods-licenses.yaml -f ../tn-utilfiles-cm.yaml -f tn-importexportscript-cm.yaml -f tn-assetexport-job.yaml
--- pipeline/common.lib/import-task.sh
+++ pipeline/common.lib/import-task.sh
@@ -8,9 +8,9 @@ MINOR=`sed -n 's/^MINOR=\(.*\)/\1/p' < ../../version.txt`
 PATCH=`sed -n 's/^PATCH=\(.*\)/\1/p' < ../../version.txt`
 VERSION=v${MAJOR}.${MINOR}.${PATCH}
 # Modify the k8s job name with release iteration and apply the k8s job specifications 
-sed -i "s/<TAG>/${VERSION}/" ../../manifests/jobs/tn-assetimport-job.yaml
-sed -i "s/<TAG>/${VERSION}/" ../../manifests/jobs/tn-importexportscript-cm.yaml
-sed -i "s/<TAG>/${VERSION}/" consolidated/TNImport.xml
+sed -i "s/<TAG>/${VERSION}/g" ../../manifests/jobs/tn-assetimport-job.yaml
+sed -i "s/<TAG>/${VERSION}/g" ../../manifests/jobs/tn-importexportscript-cm.yaml
+sed -i "s/<TAG>/${VERSION}/g" consolidated/TNImport.xml
 
 
 kubectl create configmap tn-dataload-cm --from-file=consolidated/TNImport.xml
